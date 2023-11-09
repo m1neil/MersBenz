3105910246a20bc35bee6cca8e8a8fb87ca0b89c
Eric Alekseyev <ericalex0207@gmail.com>
2023-11-09 21:22:54 +0200

    some changes

--- app/src/main/java/com/example/mersbens/MenuCarsActivity.java
+++ app/src/main/java/com/example/mersbens/MenuCarsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MenuCarsActivity extends AppCompatActivity {
 
